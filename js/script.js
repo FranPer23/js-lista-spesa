65853c69bd9fa854da4b0540df5e35174e87ce49
FranPer23 <francescaprt@yahoo.com>
2023-03-16 09:47:53 +0100

    modifica lista

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-const shoppingList = ["a", "b", "c", "d", "e", "f"];
+const shoppingList = ['a', 'b', 'c', 'd', 'e', 'f'];
 console.log(shoppingList);
 
 let listElem = 0;
-
+const list = document.querySelector("ul")
 while (listElem < shoppingList.length) {
     console.log(shoppingList[listElem]);
+    const item = shoppingList[listElem]
+    list.innerHTML += `<li>${item}</li>`
     listElem++;
 }
 
-const ul = document.querySelector('ul')
-ul.innerHTML += `<li>${shoppingList}</li>`
+
